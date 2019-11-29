2733e9dd9510183fb17237c52de2b75d03ad70b7
Juichi Matsuki <matsuki@cba-japan.com>
2019-11-29 15:31:59 +0900

    Resolve error

--- app/classes/houndify.rb
+++ app/classes/houndify.rb
@@ -14,9 +14,10 @@ class Houndify
     Rails.logger.debug("Houndify-initialize")
     
     @clientID = Rails.application.secrets.houndify_client_id
+    Rails.logger.debug("Houndify-initialize:houndify_client_id="+@clientID)
 #    @clientKey = Base64.urlsafe_decode64(Rails.application.secrets.houndify_client_secret)
-    @clientKey = Rails.application.secrets.houndify_client_secret
     Rails.logger.debug("Houndify-initialize:houndify_client_secret="+@clientKey)
+    @clientKey = Rails.application.secrets.houndify_client_secret
     @userID = userID
     @hostname = hostname
     @proxyHost = proxyHost
