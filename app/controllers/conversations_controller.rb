88d4b871b6ba0dc957123a16fcc53e73b6949781
Juichi Matsuki <matsuki@cba-japan.com>
2019-11-29 10:06:54 +0900

    Add logs

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -8,6 +8,7 @@ class ConversationsController < ApplicationController
     # Grabs the exisitng single-use token from the database (which we just created) and a necessary api key from YAML and supplies them to the front-end
     # There are other ways to do this - you could use AJAX directly from the front-end and return some values stored in a database somewhere, or grab a file
     # from an online bucket like S3 which contains the parameters - single use has to be instantiated every time, which makes my method more palatable for Rails
+    logger.debug 'ConversationsController-index'
     
     @token = Conversation.first.token
     @api_key = Rails.application.secrets.fm_api_key
@@ -16,6 +17,7 @@ class ConversationsController < ApplicationController
   def create
     # Change the second parameter to another NLP provider in order to query against that provider
     # You could also implement a custom cascading check against multiple NLP providers.
+    logger.debug 'ConversationsController-create'
 
     orchestration = Orchestration.new(params, "Houndify")
     response = orchestration.orchestrate
