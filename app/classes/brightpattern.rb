bdf50aae9cbeb2622d2577328eb32205402f834a
Juichi Matsuki <matsuki@cba-japan.com>
2019-12-02 16:50:03 +0900

    curlクラス追加

--- app/classes/brightpattern.rb
+++ app/classes/brightpattern.rb
@@ -2,14 +2,18 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
+require_relative "./curl_builder.rb"
+
 class Brightpattern
   def initialize
-        Rails.logger.debug 'Brightpattern-initialize'
+    Rails.logger.debug 'Brightpattern-initialize'
+    
+    c = CurlBuilder.new
     
   end
   
   def query_brightpattern(query)
-        Rails.logger.debug 'Brightpattern-query_brightpattern'
+    Rails.logger.debug 'Brightpattern-query_brightpattern'
     
   end
 end
--- app/classes/orchestration.rb
+++ app/classes/orchestration.rb
@@ -2,14 +2,15 @@ class Orchestration
     attr_accessor :query, :conversation_state, :location, :partner, :response
 
     def initialize(params, partner)
-        @query = params["fm-question"] # string, query from the STT engine of UneeQ
+       Rails.logger.debug 'Orchestration-initialize'
+
+      @query = params["fm-question"] # string, query from the STT engine of UneeQ
         @conversation_state = params["fm-conversation"].blank? ? nil : params["fm-conversation"] # Maintain conversation state between utterances
         @location = params["fm-custom-data"].blank? ? {} : JSON.parse(params["fm-custom-data"])
         @partner = partner # string, the name of the partner company we reach out to
         @response = nil
         @bp = Brightpattern.new
       
-        Rails.logger.debug 'Orchestration-initialize'
     end
 
     def orchestrate
