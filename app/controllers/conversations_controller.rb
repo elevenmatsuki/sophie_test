8a91d02ef852d75a8cb40da9525f32c23637fa92
Juichi Matsuki <matsuki@cba-japan.com>
2019-12-23 07:44:01 +0100

    関数整理

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -25,9 +25,9 @@ class ConversationsController < ApplicationController
     logger.debug("ConversationsController-create")
     logger.debug params.inspect
 
-#    orchestration = Orchestration.new(params, "BrightPattern")
+ #   orchestration = Orchestration.new(params, "BrightPattern")
     orchestration = Orchestration.new(params, "Houndify")
-#    response = orchestration.orchestrate
+     response = orchestration.orchestrate
     
 #    orchestration.send_chat
     
