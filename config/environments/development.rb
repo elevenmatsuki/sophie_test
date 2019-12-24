d46a4794aec9f431ef371c173b872b539193fad2
Juichi Matsuki <matsuki@cba-japan.com>
2019-12-24 14:55:13 +0100

    Time処理-Javascriptテスト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  config.logger = ActiveSupport::Logger.new("log/development.log")
+  config.logger = ActiveSupport::Logger.new('log/development.log', 'daily')
 #  config.logger = Logger.new(STDOUT)
   config.log_level = :debug
   config.logger.formatter = ::Logger::Formatter.new
