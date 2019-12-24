7ceb316b2b9f70c68f8bb994a7e33c87338b1c07
Juichi Matsuki <matsuki@cba-japan.com>
2019-12-24 11:57:50 +0100

    設定ファイルから読み込み

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,5 +78,11 @@ Rails.application.configure do
 #  config.web_console.whitelisted_ips = '106.154.123.0/16'
 #  config.web_console.whitelisted_ips = '106.154.123.74'  
   config.action_controller.allow_forgery_protection = false  
+
+  ### BrightPattern ###
+  #CBADEV
+  config.x.brightpattern.hostname = cbadev.brightpattern.com
+  config.x.brightpattern.appId = e7926a805d904b11a21dbe114beaf098
+  config.x.brightpattern.clientId = WebChat
   
 end
