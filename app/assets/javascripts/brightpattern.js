0201283ee8a62750c25829692173941653f2dc7f
Juichi Matsuki <matsuki@cba-japan.com>
2020-01-20 14:03:42 +0100

    DEBUG

--- app/assets/javascripts/brightpattern.js
+++ app/assets/javascripts/brightpattern.js
@@ -65,10 +65,10 @@ var BrightPattern = function(){
     this.sendChat = function(msg, chat_id, callback){
         console.log("sendChat");
         body = [{
-          "events": {
-              "event":"chat_session_message",
+          "events": [{
+              "event": "chat_session_message",
               "msg": msg
-            }
+            }]
         }];
         json_body = JSON.stringify(body);
         console.log(chat_id);
