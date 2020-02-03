f5c40ae163e7effb0e3720c02560bb8014850974
Juichi Matsuki <matsuki@cba-japan.com>
2020-02-03 22:57:06 +0100

    WatsonDEBUG

--- app/classes/watson.rb
+++ app/classes/watson.rb
@@ -45,8 +45,8 @@ class Watson
 
     uri = URI.parse("https://gateway-tok.watsonplatform.net/assistant/api/v2/assistants/e65ae379-0d2d-4cd7-800c-c30da8d805bf/sessions?version=2019-02-28")
     request = Net::HTTP::Post.new(uri)
-#    request.basic_auth("apikey", "UGlBuwv0OEzF_klK07sGG6O2yGh4OZbcfWQN93_ZTqpB")
-    request["Authorization"] = "apikey:UGlBuwv0OEzF_klK07sGG6O2yGh4OZbcfWQN93_ZTqpB"
+    request.basic_auth("apikey", "UGlBuwv0OEzF_klK07sGG6O2yGh4OZbcfWQN93_ZTqpB")
+#    request["Authorization"] = "apikey:UGlBuwv0OEzF_klK07sGG6O2yGh4OZbcfWQN93_ZTqpB"
     
     req_options = {
       use_ssl: uri.scheme == "https",
@@ -64,6 +64,9 @@ class Watson
     Rails.logger.debug("---REQUEST---")
     Rails.logger.debug request
     Rails.logger.debug uri.inspect
+    Rails.logger.debug request.hostname
+    Rails.logger.debug request.port
+    Rails.logger.debug req_options
 
     Rails.logger.debug("---RESPONSE---")
     if response
