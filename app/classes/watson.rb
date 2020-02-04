b65b0287ec0364e35e8e67646245679f7e061831
Juichi Matsuki <matsuki@cba-japan.com>
2020-02-04 09:51:09 +0100

    WatsonDEBUG

--- app/classes/watson.rb
+++ app/classes/watson.rb
@@ -43,8 +43,8 @@ class Watson
 #    request.content_type = "application/json"
 #    request["Authorization"] = "MOBILE-API-140-327-PLAIN appId=\"" + appId + "\", clientId=\"" + clientId + "\""
 
-    uri = URI.parse("https://gateway-tok.watsonplatform.net/assistant/api/v2/assistants/e65ae379-0d2d-4cd7-800c-c30da8d805bf/sessions")
-#    uri = URI.parse("https://gateway-tok.watsonplatform.net/assistant/api/v2/assistants/e65ae379-0d2d-4cd7-800c-c30da8d805bf/sessions?version=2019-02-28")
+#    uri = URI.parse("https://gateway-tok.watsonplatform.net/assistant/api/v2/assistants/e65ae379-0d2d-4cd7-800c-c30da8d805bf/sessions")
+    uri = URI.parse("https://gateway-tok.watsonplatform.net/assistant/api/v2/assistants/e65ae379-0d2d-4cd7-800c-c30da8d805bf/sessions?version=2019-02-28")
 #    uri = URI.parse("https://apikey:UGlBuwv0OEzF_klK07sGG6O2yGh4OZbcfWQN93_ZTqpB@gateway-tok.watsonplatform.net/assistant/api/v2/assistants/e65ae379-0d2d-4cd7-800c-c30da8d805bf/sessions?version=2019-02-28")
 #    request = Net::HTTP::Post.new(uri)
 #    request.basic_auth 'apikey', 'UGlBuwv0OEzF_klK07sGG6O2yGh4OZbcfWQN93_ZTqpB'
@@ -53,11 +53,11 @@ class Watson
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.scheme === "https"
-    headers = { "Authorization" => "Basic YXBpa2V5OlVHbEJ1d3YwT0V6Rl9rbEswN3NHRzZPMnlHaDRPWmJjZldRTjkzX1pUcXBC", "Content-Type" => "application/json" }
-    params = { "version" => "2019-02-28" }
+    headers = { "Authorization" => "Basic YXBpa2V5OlVHbEJ1d3YwT0V6Rl9rbEswN3NHRzZPMnlHaDRPWmJjZldRTjkzX1pUcXBC"}
+#    params = { "version" => "2019-02-28" }
 #    headers = { "Content-Type" => "application/json" }
     request = Net::HTTP::Post.new(uri.path)
-    request.set_form_data(params)
+#    request.set_form_data(params)
     request.initialize_http_header(headers)
     
 #    req_options = {
