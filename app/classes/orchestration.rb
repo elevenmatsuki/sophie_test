7c7ef7117408e29558b471c3b1c8a667cd19004b
Juichi Matsuki <matsuki@cba-japan.com>
2020-02-06 20:39:34 +0100

    DEBUG

--- app/classes/orchestration.rb
+++ app/classes/orchestration.rb
@@ -22,7 +22,8 @@ class Orchestration
       when "BrightPattern"
         @bp = Brightpattern.new
       when "Watson"
-        return Watson.new.query_sendchat(@query)
+#        return Watson.new.query_sendchat(@query)
+        return Watson.new
       else
           return nil
       end
