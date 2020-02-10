7909d479f4ece5a890ac39fa6e0c34b1057edcbc
Juichi Matsuki <matsuki@cba-japan.com>
2020-02-10 16:13:10 +0100

    DEBUG

--- app/classes/brightpattern.rb
+++ app/classes/brightpattern.rb
@@ -240,7 +240,7 @@ class Brightpattern
     Rails.logger.debug headers
     Rails.logger.debug body
     Rails.logger.debug("===RESPONSE===")
-    Rails.logger.debug response
+    Rails.logger.debug response.inspect
     Rails.logger.debug response.body
   end
   
